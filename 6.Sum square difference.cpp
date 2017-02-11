78498b5ce6fb3f68387d589ebd186654320185b8
MarioCode <antoxam07@mail.ru>
2017-02-11 19:33:54 +0300

    Sum square difference

---
+++ 6.Sum square difference.cpp
@@ -0,0 +1,21 @@
+#include "math.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  long long int sum = 0, sumSQ = 0;
+  int N = 0;
+
+  cin >> N;
+
+  for (int i = 1; i <= N; ++i) {
+    sumSQ += pow(i, 2);
+    sum += i;
+  }
+
+  cout << (int)pow(sum, 2) - sumSQ;
+
+  return 0;
+}
